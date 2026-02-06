5bbc780527663697282ac91111173348cd6ab82e
PaulaSalazar08 <paulasm1710@gmail.com>
2026-02-06 10:08:01 +0100

    Add files via upload

---
+++ hola_mundo.cpp
@@ -0,0 +1,12 @@
+/*Programa hola mundo*/
+//Esto tambien es un comentario
+//main() es el CUERPO PRINCIPAL, es una funcion
+//instruccion al preprocesador
+#include<stdio.h>
+
+int main(){
+    char salir;
+    printf("HOLA MUNDO");
+    scanf("%c",&salir);
+   
+}
