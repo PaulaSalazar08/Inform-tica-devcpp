a6483bd749c41f3113f956275fc5054dcd95d638
PaulaSalazar08 <paulasm1710@gmail.com>
2026-02-10 11:24:35 +0100

    Otra manera num mayor/menor

---
+++ num_mayor_menor_2.cpp
@@ -0,0 +1,37 @@
+/*Escribe un programa que lea números enteros y devuelva el mayor y el menor */
+
+#include<stdio.h>
+
+int main(){
+int n[3];//esto es un vector de tres posiciones
+int mayor;
+int cont;
+//Pido al usuario que introduzca esos números
+//cont++ es equivalente cont=cont+1
+for(cont=0;cont<3;cont++){
+    printf("\nIntroduce el numero %i: ",cont);
+    scanf(" %i",&n[cont]);
+}
+//Mostrar los números almacenados en el vector
+printf("\nLos numeros almacenados: ");
+for(cont=0;cont<3;cont++){
+    printf("\n n[%i]=%i",cont,n[cont]);
+}
+//Calcula la suma
+
+/*
+mayor=n1;
+//printf("\nHas introducido los numeros %i, %i y %i",n1,n2,n3);
+if(n1>n2){
+    mayor=n1;    
+}
+else{
+    mayor=n2;
+}
+if(n3>mayor){
+    mayor=n3;
+}
+printf("El mayor es el %i",mayor);*/
+return 0;
+
+}
